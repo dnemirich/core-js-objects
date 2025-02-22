6e3a9f3015ac3787da46e7f26a4ef837cba74839
dnemirich <dnemirich@gmail.com>
2025-02-22 19:49:34 +0100

    feat: satisfy 15/20 tests

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -127,8 +127,19 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (Object.keys(lettersObject).length === 0) return '';
+
+  const maxVal = Math.max(...Object.values(lettersObject).flat(), 0);
+  const res = new Array(maxVal + 1).fill('-');
+
+  Object.entries(lettersObject).forEach(([key, values]) => {
+    values.forEach((val) => {
+      res[val] = key;
+    });
+  });
+
+  return res.join('');
 }
 
 /**
