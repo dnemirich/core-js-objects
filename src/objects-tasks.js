c2a99c26be2077695b15250c7646cf8ebc9d6647
dnemirich <dnemirich@gmail.com>
2025-02-22 20:20:06 +0100

    feat: satisfy 17/20 tests

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -217,8 +217,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = {};
+  Object.setPrototypeOf(obj, proto);
+  Object.assign(obj, JSON.parse(json));
+  return obj;
 }
 
 /**
